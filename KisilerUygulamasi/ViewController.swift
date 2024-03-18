69b086537446b560cad36766de2e6827e7d8c225
iremsupalaa <iremsuupalaa@gmail.com>
2024-03-18 14:20:41 +0300

    Initial Commit

---
+++ KisilerUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasi
+//
+//  Created by İremsu Pala on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
